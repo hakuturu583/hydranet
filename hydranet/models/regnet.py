f6e4fc1fbdf7de37c015fe1801d4573acb106afb
Masaya Kataoka <ms.kataoka@gmail.com>
2022-10-12 02:11:12 +0900

    add command line option
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- hydranet/models/regnet.py
+++ hydranet/models/regnet.py
@@ -1,24 +1,27 @@
-import math
-from collections import OrderedDict
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from functools import partial
 from typing import Callable, Optional, Any
 
 from torchvision.models.regnet import (
     BlockParams,
-    SimpleStemIN,
-    ResBottleneckBlock,
-    AnyStage,
+    WeightsEnum,
+    regnet_y_400mf,
     RegNet_Y_400MF_Weights,
 )
-from torchvision.models._api import WeightsEnum
+from torchvision.models.regnet import RegNet
+
 from torchvision.models._utils import _ovewrite_named_param
-from torchvision.utils import _log_api_usage_once
 
 from torch import nn, Tensor
 import torch.onnx
 
+import argparse
+import os
 
-class RegNet(nn.Module):
+
+class RegNetHead(RegNet):
     def __init__(
         self,
         block_params: BlockParams,
@@ -29,66 +32,15 @@ class RegNet(nn.Module):
         norm_layer: Optional[Callable[..., nn.Module]] = None,
         activation: Optional[Callable[..., nn.Module]] = None,
     ) -> None:
-        super().__init__()
-        _log_api_usage_once(self)
-
-        if stem_type is None:
-            stem_type = SimpleStemIN
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        if block_type is None:
-            block_type = ResBottleneckBlock
-        if activation is None:
-            activation = nn.ReLU
-
-        # Ad hoc stem
-        self.stem = stem_type(3, stem_width, norm_layer, activation)  # width_in
-
-        current_width = stem_width
-
-        blocks = []
-        for (
-            i,
-            (width_out, stride, depth, group_width, bottleneck_multiplier),
-        ) in enumerate(block_params._get_expanded_params()):
-            blocks.append(
-                (
-                    f"block{i+1}",
-                    AnyStage(
-                        current_width,
-                        width_out,
-                        stride,
-                        depth,
-                        block_type,
-                        norm_layer,
-                        activation,
-                        group_width,
-                        bottleneck_multiplier,
-                        block_params.se_ratio,
-                        stage_index=i + 1,
-                    ),
-                )
-            )
-
-            current_width = width_out
-
-        self.trunk_output = nn.Sequential(OrderedDict(blocks))
-
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(in_features=current_width, out_features=num_classes)
-
-        # Performs ResNet-style weight initialization
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                # Note that there is no bias due to BN
-                fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                nn.init.normal_(m.weight, mean=0.0, std=math.sqrt(2.0 / fan_out))
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.ones_(m.weight)
-                nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, mean=0.0, std=0.01)
-                nn.init.zeros_(m.bias)
+        super().__init__(
+            block_params=block_params,
+            num_classes=num_classes,
+            stem_width=stem_width,
+            stem_type=stem_type,
+            block_type=block_type,
+            norm_layer=norm_layer,
+            activation=activation,
+        )
 
     def forward(self, x: Tensor) -> Tensor:
         x = self.stem(x)
@@ -100,30 +52,10 @@ class RegNet(nn.Module):
 
         return x
 
-    def to_onnx(self, filename="onnx/regnet.onnx"):
+    def to_onnx(self, filename=os.path.dirname(__file__) + "/../onnx/regnet.onnx"):
         self.eval()
         dummy_input = torch.randn((1, 3, 224, 224))
         torch.onnx.export(self, dummy_input, filename, verbose=True)
-
-
-def _regnet(
-    block_params: BlockParams,
-    weights: Optional[WeightsEnum],
-    progress: bool,
-    **kwargs: Any,
-) -> RegNet:
-    if weights is not None:
-        _ovewrite_named_param(kwargs, "num_classes", len(weights.meta["categories"]))
-
-    norm_layer = kwargs.pop(
-        "norm_layer", partial(nn.BatchNorm2d, eps=1e-05, momentum=0.1)
-    )
-    model = RegNet(block_params, norm_layer=norm_layer, **kwargs)
-
-    if weights is not None:
-        model.load_state_dict(weights.get_state_dict(progress=progress))
-
-    return model
 
 
 def regnet_y_400mf(
@@ -131,13 +63,64 @@ def regnet_y_400mf(
     weights: Optional[RegNet_Y_400MF_Weights] = None,
     progress: bool = True,
     **kwargs: Any,
-) -> RegNet:
+) -> RegNetHead:
+    """
+    Constructs a RegNetY_400MF architecture from
+    `Designing Network Design Spaces <https://arxiv.org/abs/2003.13678>`_.
+    Args:
+        weights (:class:`~torchvision.models.RegNet_Y_400MF_Weights`, optional): The pretrained weights to use.
+            See :class:`~torchvision.models.RegNet_Y_400MF_Weights` below for more details and possible values.
+            By default, no pretrained weights are used.
+        progress (bool, optional): If True, displays a progress bar of the download to stderr. Default is True.
+        **kwargs: parameters passed to either ``torchvision.models.regnet.RegNet`` or
+            ``torchvision.models.regnet.BlockParams`` class. Please refer to the `source code
+            <https://github.com/pytorch/vision/blob/main/torchvision/models/regnet.py>`_
+            for more detail about the classes.
+    .. autoclass:: torchvision.models.RegNet_Y_400MF_Weights
+        :members:
+    """
+    weights = RegNet_Y_400MF_Weights.verify(weights)
+
     params = BlockParams.from_init_params(
         depth=16, w_0=48, w_a=27.89, w_m=2.09, group_width=8, se_ratio=0.25, **kwargs
     )
     return _regnet(params, weights, progress, **kwargs)
 
 
+def _regnet(
+    block_params: BlockParams,
+    weights: Optional[WeightsEnum],
+    progress: bool,
+    **kwargs: Any,
+) -> RegNetHead:
+    if weights is not None:
+        _ovewrite_named_param(kwargs, "num_classes", len(weights.meta["categories"]))
+
+    norm_layer = kwargs.pop(
+        "norm_layer", partial(nn.BatchNorm2d, eps=1e-05, momentum=0.1)
+    )
+    model = RegNetHead(block_params, norm_layer=norm_layer, **kwargs)
+
+    if weights is not None:
+        model.load_state_dict(weights.get_state_dict(progress=progress))
+
+    return model
+
+
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Python script for RegNet model")
+    parser.add_argument("cmd", choices=["print", "onnx"])
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="output file path",
+        default=os.path.dirname(__file__) + "/../onnx/regnet.onnx",
+    )
+    parser.parse_args()
+    args = parser.parse_args()
     net = regnet_y_400mf()
-    net.to_onnx()
+    if args.cmd == "print":
+        print(net)
+    elif args.cmd == "onnx":
+        net.to_onnx(args.output)
