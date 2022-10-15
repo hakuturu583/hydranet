b660202b529a8f3d3d8df73dca5f4f455f8499a9
Masaya Kataoka <ms.kataoka@gmail.com>
2022-10-15 14:39:44 +0900

    add bifpn for neck
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- hydranet/models/hydranet.py
+++ hydranet/models/hydranet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from turtle import back
 from bifpn import BiFPN
 from regnet import RegNet, regnet_y_400mf, RegNet_Y_400MF_Weights
 from torch import nn
@@ -10,8 +11,13 @@ class Hydranet(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.backbone = regnet_y_400mf(weights=RegNet_Y_400MF_Weights.IMAGENET1K_V1)
+        backbone_output_shape = self.backbone.get_output_shapes()
+        for shape in backbone_output_shape:
+            self.neck = nn.Sequential(
+                BiFPN(shape[1]), BiFPN(shape[1]), BiFPN(shape[1]), BiFPN(shape[1])
+            )
 
 
 if __name__ == "__main__":
     net = Hydranet()
-    print(net)
+    # print(net)
